df3a615831e8e436d1821b277a51c313dac27c46
DESKTOP-399M2SR\Admin <adityapandey0264@gmail.com>
2023-03-13 23:18:20 +0530

    The Observer Pattern

---
+++ Project_StudyingPatterns/Assets/Scripts/Health.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float fullHealth = 100f;
+    [SerializeField] float drainPerSecond = 2f;
+
+    float currentHealth = 0;
+
+    private void Awake()
+    {
+        resetHealth();
+        StartCoroutine(healthDrain());
+    }
+
+    public void resetHealth()
+    {
+        currentHealth = fullHealth;
+    }
+    public float getHealth()
+    {
+        return currentHealth;
+    }
+
+    IEnumerator healthDrain()
+    {
+        while(currentHealth>0)
+        {
+            currentHealth -= drainPerSecond;
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+}
---
+++ Project_StudyingPatterns/Assets/Scripts/debugger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugger : MonoBehaviour
+{
+     IEnumerator Start()
+    {
+        Health health = GetComponent<Health>();
+        Level level = GetComponent<Level>();
+        while(true)
+        {
+            yield return new WaitForSeconds(1f);
+            Debug.Log($"Exp:{level.GetExperience()},Level:{level.GetLevel()},Health:{health.getHealth()}");
+        }
+    }
+}
